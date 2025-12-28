450b819dd659523b02fdae432df47ccbe72b4107
Harsha Nandhan Reddy <112433366+Harshanandhan@users.noreply.github.com>
2025-12-27 23:35:33 -0500

    Delete test_detector.sh

--- test_detector.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Quick test script
-
-echo "Testing Vulnerable Contract..."
-python detector.py --file sample_contracts/vulnerable.sol
-
-echo ""
-echo "Testing Secure Contract..."
-python detector.py --file sample_contracts/secure.sol
